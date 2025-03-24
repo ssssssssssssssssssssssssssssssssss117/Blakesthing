7d7e6344c7977a7358ed240b8a9633610cf06141
ssssssssssssssssssssssssssssssssss117 <blakeingham@garincollege.nz>
2025-03-24 01:54:23 +0000

    Krabby Patty

---
+++ script.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent('videoplayer', {
+  init: function() {
+    let videosource = document.querySelector('#videoclip');
+    let videoplay = () => {
+      videosource.play();
+    }
+    this.el.addEventListener('click', videoplay);
+  }
+});
+
+AFRAME.registerComponent('videopauser', {
+  init: function() {
+    let videosource = document.querySelector('#videoclip');
+    let videopause = () => {
+      videosource.pause();
+    }
+    this.el.addEventListener('click', videopause);
+  }
+});
+
